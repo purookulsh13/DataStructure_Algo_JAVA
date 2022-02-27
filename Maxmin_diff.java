4e88c840ce5c1b325402649c8325605bfa0a7cca
Puroo Kulshrestha <75776924+purookulsh13@users.noreply.github.com>
2022-02-28 05:27:39 +0530

    Add files via upload

---
+++ Maxmin_diff.java
@@ -0,0 +1,19 @@
+public class Maxmin_diff {
+    public static void main(String args[]) {
+        int arr[] = { 20, 10, 40, 50, 45 };
+        int max_value = arr[0];
+        int min_value = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max_value) {
+                max_value = arr[i];
+            } else if (arr[i] < min_value) {
+                min_value = arr[i];
+            }
+        }
+        System.out.println("max_value in array is = " + max_value);
+        System.out.println("min_value in array is = " + min_value);
+        System.out.println("Difference between Max_value and Min_value is = " + (max_value - min_value));
+
+    }
+
+}
