e8d9d0747df8dc565ff47465bfc842a6ed5bfb6a
Puroo Kulshrestha <75776924+purookulsh13@users.noreply.github.com>
2022-02-28 01:49:25 +0530

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Stack_13 {
+    public static void main(String args[]) {
+        Stack<Integer> s = new Stack<>();
+        s.push(4);
+        s.push(8);
+        s.push(12);
+        System.out.println(s.pop());
+    }
+}
