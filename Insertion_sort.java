92ba085b33dcc314a732bb37d3f9fbf738d47f15
Puroo Kulshrestha <75776924+purookulsh13@users.noreply.github.com>
2022-04-09 00:52:45 +0530

    Add files via upload

---
+++ Insertion_sort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Insertion_sort {
+    static void sort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+    }
+
+    // Driver method
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = s.nextInt();
+        }
+        sort(arr);
+        printArray(arr);
+        s.close();
+    }
+}
